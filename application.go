f61d2e5b616ece47a4628258ca3e3a86e74c2318
jpincas <jon@pincas.co.uk>
2018-01-21 21:21:24 +0100

    put in new routing code but commented

--- application.go
+++ application.go
@@ -7,6 +7,10 @@ import (
 	"net/http"
 )
 
+func init() {
+	App.IndexTemplate = template.Must(template.New("index").Parse(index))
+}
+
 // Config stores configuration options for the application
 type Config struct {
 	AppPort int
@@ -14,6 +18,8 @@ type Config struct {
 
 // Application is the top-level representation of your application
 type Application struct {
+	// template for rendering index html and js
+	IndexTemplate *template.Template
 	// template for rendering framework errors
 	ErrorTemplate       *template.Template
 	Templates           *template.Template
@@ -37,9 +43,29 @@ func (app Application) Broadcast() {
 
 // Start runs the application server
 func (app Application) Start(distDirectory string) {
+
 	fs := http.FileServer(http.Dir(distDirectory))
 	http.HandleFunc("/server", handler)
 	http.Handle("/", fs)
 	log.Println("Staring gotea app server...")
 	http.ListenAndServe(fmt.Sprintf(":%v", app.Config.AppPort), nil)
+
+	// TODO:
+	// USe this code for routing
+	// http.HandleFunc("/server", handler)
+	// http.HandleFunc("/", renderIndex)
+	// log.Println("Staring gotea app server...")
+	// http.ListenAndServe(fmt.Sprintf(":%v", app.Config.AppPort), nil)
 }
+
+// func renderIndex(w http.ResponseWriter, r *http.Request) {
+
+// 	templateData := struct {
+// 		AppTitle string
+// 	}{
+// 		"gotea App",
+// 	}
+
+// 	App.IndexTemplate.Execute(w, templateData)
+
+// }
