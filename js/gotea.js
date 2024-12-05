c10bba7a702dbf6f38e145560a3b7cedf4de76f5
Dr Jonathan Pincas <jon@pincas.co.uk>
2024-12-05 19:52:58 +0100

    organised and refactored JS, don't cache builds

--- js/gotea.js
+++ js/gotea.js
@@ -1,117 +1,84 @@
 import morphdom from "morphdom";
 
+// Constants
+const SOCKET_MESSAGE = "Sent Message:";
 
 // Websockets
 const socket = new WebSocket(
-  (window.location.protocol === "https:" ? "wss://" : "ws://") + window.location.host + "/server?whence=" + document.location.pathname
+  `${window.location.protocol === "https:" ? "wss://" : "ws://"}${window.location.host}/server?whence=${document.location.pathname}`
 );
 
-// socket.onopen = () => {
-//   const path = window.location.pathname;
-//   if (path != "/") {
-//     changeRoute(path);
-//   }
-// };
-
+// Handle incoming messages from the server
 socket.onmessage = event => {
   morphdom(document.documentElement, event.data, {
     childrenOnly: true
   });
 };
 
-// DOM Swap with Morphdom
-// with the option 'childrenOnly' Morphom swaps children of the containers,
-// therefore we place the incoming HTML in a div
-// const swapDOM = (incomingHTML) => {
-//   // const el1 = document.createElement("div");
-//   // el1.innerHTML = incomingHTML;
-//   morphdom(document.getElementById(containerID), incomingHTML, {
-//     childrenOnly: true
-//   });
-// };
-
-const sendMessage = (msgString, args) => {
+// Send a message through the websocket
+const sendMessage = (message, args) => {
   const msg = {
-    message: msgString,
+    message,
     args: JSON.parse(args)
   };
-  console.log("Sending websocket message: ", msg);
+  console.log(`${SOCKET_MESSAGE}`, msg);
   socket.send(JSON.stringify(msg));
 };
 
-function submitForm(message, formID) {
+// Submit a form through the websocket
+const submitForm = (message, formID) => {
   const msg = {
     message,
     args: serializeForm(formID)
   };
-
-  console.log("Sending websocket message: ", msg);
+  console.log(`${SOCKET_MESSAGE}`, msg);
   socket.send(JSON.stringify(msg));
-}
+};
 
-function sendMessageWithValue(message, inputID) {
-  let value = document.getElementById(inputID).value;
-
-  let msg = {
+// Send a message with a value from an input field
+const sendMessageWithValue = (message, inputID) => {
+  const value = document.getElementById(inputID).value;
+  const msg = {
     message,
     args: value
   };
-
-  console.log("Sending websocket message: ", msg);
+  console.log(`${SOCKET_MESSAGE}`, msg);
   socket.send(JSON.stringify(msg));
-}
-
-window.gotea = {
-  sendMessage: sendMessage,
-  submitForm: submitForm,
-  sendMessageWithValue: sendMessageWithValue
 };
 
+// Serialize form data into an object
 const serializeForm = formID => {
   const formElements = [...document.getElementById(formID).elements];
   const TEXT_INPUT = "text";
   const CHECKBOX = "checkbox";
   const RADIO = "radio";
-  const SELECT = "SELECT"; // for some reason tagName returns uppercase name
+  const SELECT = "SELECT";
   const TEXTAREA = "TEXTAREA";
 
-  // map over array of options (elements children). If option is selected, return it's value
-  // otherwise return empty string and then filter out empty strings from the array so we end up
-  // with array of selected options.
   const buildSelectArray = select => [...select.children]
     .map(option => (option.selected ? option.value : ""))
     .filter(value => value.length > 0);
 
-  // if select has multiple attribute, build array of selected options,
-  // otherwise, for dropdown for example, return select's value
   const handleSelect = select =>
     select.multiple ? buildSelectArray(select) : select.value;
 
-  // reduce over form elements and check against their type (for inputs) and tagName for other tags
-  // (textarea/select etc). Return data shaped specifically for structs on server-side, so they are easy
-  // to decode
   return formElements.reduce((acc, el) => {
     switch (el.tagName) {
       case SELECT:
-        // select may have multiple values, check if it is a case and return proper value (array or string)
         acc[el.name] = handleSelect(el);
         break;
       case TEXTAREA:
-        // simple string to field name assignment
         acc[el.name] = el.value;
         break;
     }
     switch (el.type) {
-      // simple string to field name assignment
       case TEXT_INPUT:
         acc[el.name] = el.value;
         break;
       case CHECKBOX:
-        // see if checkbox is checked and assign bool to it's name.
         acc[el.name] = el.checked;
         break;
       case RADIO:
-        // if radio is checked, assign it's value (string) to it's name
         if (el.checked) {
           acc[el.name] = el.value;
         }
@@ -121,34 +88,39 @@ const serializeForm = formID => {
   }, {});
 };
 
-function changeRoute(route) {
+// Change the route and notify the server
+const changeRoute = route => {
   history.pushState({}, "", route);
-
   const msg = {
     message: "CHANGE_ROUTE",
     args: route
   };
-
-  console.log("Sending websocket message: ", msg);
+  console.log(`${SOCKET_MESSAGE}`, msg);
   socket.send(JSON.stringify(msg));
-}
+};
 
+// Expose functions to the global window object
+window.gotea = {
+  sendMessage,
+  submitForm,
+  sendMessageWithValue
+};
 
-// For some reason only work when place on Window
-window.addEventListener('popstate', function (event) {
+// Handle browser back/forward navigation
+window.addEventListener('popstate', event => {
   const msg = {
     message: "CHANGE_ROUTE",
     args: document.location.pathname,
   };
-
-  console.log("Sending websocket message: ", msg);
+  console.log(`${SOCKET_MESSAGE}`, msg);
   socket.send(JSON.stringify(msg));
 });
 
+// Intercept link clicks and handle routing
 document.addEventListener(
   "click",
   e => {
-    if (e.target.tagName == 'A' && /external/.test(e.target.className) == false) {
+    if (e.target.tagName === 'A' && !/external/.test(e.target.className)) {
       e.preventDefault();
       changeRoute(e.target.getAttribute("href"));
       return false;
@@ -156,11 +128,3 @@ document.addEventListener(
   },
   false
 );
-
-export default {
-  sendMessage,
-  submitForm,
-  sendMessageWithValue,
-  serializeForm,
-  changeRoute
-};
